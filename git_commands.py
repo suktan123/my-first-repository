7a2d4a3c4287e692675cfd75e0c0d37d6e12988c
sultan <orozbekovsultan49@gmail.com>
2023-08-01 19:29:56 +0600

    first commit

---
+++ git_commands.py
@@ -0,0 +1,14 @@
+# git - система управления версиями кода
+# github - сайт, который дает хранилище для кода
+# также есть gitlab, bitbucket
+
+# репозиторий - папка с проектом с которой может работать git
+
+# ssh-keygen - создание ключа
+
+
+# git config --global user.name "your-name"
+# get config --global user.email "your-email"
+# указание своих данных
+
+# git init - превратить папку в репозиторий и создает скрытую папку .git
